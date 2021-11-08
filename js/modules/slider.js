a543256165ac58197074c8e353b04fc0fc79a16e
Vladyslav <notkiller999@gmail.com>
2021-11-08 22:29:30 +0200

    Сделал модули

---
+++ js/modules/slider.js
@@ -0,0 +1,155 @@
+function slider() {
+    // Slider
+    const current = document.querySelector('#current'),
+        slider = document.querySelector('.offer__slider'),
+        total = document.querySelector('#total'),
+        slides = document.querySelectorAll('.offer__slider-wrapper .offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function currentZero() {
+        current.textContent = getZero(slideIndex);
+    }
+    currentZero();
+
+    function slidesTransform() {
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    }
+
+    function widthNoPX(someWidth) {
+        return +someWidth.replace(/\D/g, '');
+    }
+
+    total.textContent = getZero(slides.length);
+
+
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15% ;
+        margin-left: 15% ;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function dotActive() {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+
+    next.addEventListener('click', () => {
+        if (offset == widthNoPX(width) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += widthNoPX(width);
+        }
+        slidesTransform();
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        currentZero();
+        dotActive();
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+
+            offset = widthNoPX(width) * (slides.length - 1);
+
+        } else {
+            offset -= widthNoPX(width);
+        }
+        slidesTransform();
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        currentZero();
+
+        dotActive();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesTransform();
+            currentZero();
+            dotActive();
+
+        });
+    });
+}
+
+module.exports = slider;
